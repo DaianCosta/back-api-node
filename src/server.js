07b6d1500964e7d586542f6b4afb7c7caeabb952
daiancosta <desenvolvimentodaian@mail.com>
2019-04-19 21:53:31 -0300

    ajustes

--- src/server.js
+++ src/server.js
@@ -32,5 +32,5 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'));
 
-server.listen(3001);//protocolo http e socket
+server.listen(process.env.PORT || 3001);//protocolo http e socket
 
